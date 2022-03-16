62b621ca3fddcdca95759125fe24a8711c449b43
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-03-16 16:41:00 +0700

    template render

--- frontend/views.py
+++ frontend/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
+from django.views.generic import View
 # Create your views here.
 
 
-class HomeView(APIView):
-    renderer_class = [TemplateHTMLRenderer]
-    template_name = 'index.html'
-
+class HomeView(View):
     def get(self, request):
-        return Response()
+        return render(request, 'index.html')
