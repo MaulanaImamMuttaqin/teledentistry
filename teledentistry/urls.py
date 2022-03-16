9ed1d256e99ce8152c5bc18a030727347b3165d3
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-03-16 16:28:51 +0700

    fix

--- teledentistry/urls.py
+++ teledentistry/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include("auth_api.urls")),
     path('api/', include("api.urls")),
+    path('', include("frontend.urls"))
 ]
