2a2de9bcdae91db14b24936f607b9f0fdf53f4dd
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-03-29 22:32:05 +0700

    asldjkasd

--- auth_api/views.py
+++ auth_api/views.py
@@ -42,4 +42,6 @@ class EmailTokenObtainPairView(TokenObtainPairView):
             }
             return Response(response, status=status.HTTP_200_OK)
 
+        return Response({"error" : True}, status=status.HTTP_400_BAD_REQUEST)
+
 # class LoginView()
