d4b3c54e8cb476bfabf5a9839f994b67adbdef7e
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-03-29 22:28:38 +0700

    kjldfjkldf

--- auth_api/views.py
+++ auth_api/views.py
@@ -1,7 +1,11 @@
+
+from asyncio.windows_events import NULL
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import UserSerializer, TokenObtainPairSerializer
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
@@ -19,5 +23,23 @@ class RegisterView(generics.CreateAPIView):
 class EmailTokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        # try:
+        #     serializer.is_valid(raise_exception=True)
+        # except TokenError as e:
+        #     raise InvalidToken(e.args[0])
+
+        # return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+        if serializer.is_valid(raise_exception= True):
+            response = {
+                "error" : NULL,
+                "code" : 200,
+                "data" : serializer.validated_data,
+                "message" : "Success"
+            }
+            return Response(response, status=status.HTTP_200_OK)
 
 # class LoginView()
