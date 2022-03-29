f3c19c86a1f1e97535c39137f0f7fad6ec7b6552
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-03-29 22:50:45 +0700

    change register response

--- auth_api/views.py
+++ auth_api/views.py
@@ -10,13 +10,18 @@ class RegisterView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     
-    
-    # def post(self, *args, **kwargs):
-    #     serializer = UserSerializer(data=self.request.data)
-    #     if serializer.is_valid():
-    #         get_user_model().objects.create_user(**serializer.validated_data)
-    #         return Response(status=HTTP_201_CREATED)
-    #     return Response(status=HTTP_400_BAD_REQUEST, data={'errors': serializer.errors})
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        response = {
+                "error" : None,
+                "code" : 200,
+                "data" : serializer.data,
+                "message" : "Success"
+            }
+        return Response(response, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class EmailTokenObtainPairView(TokenObtainPairView):
@@ -39,6 +44,7 @@ class EmailTokenObtainPairView(TokenObtainPairView):
                 "data" : serializer.validated_data,
                 "message" : "Success"
             }
+
             return Response(response, status=status.HTTP_200_OK)
 
         return Response({"error" : True}, status=status.HTTP_400_BAD_REQUEST)
