12f5b1748aa36a368dce8620b1c5058814da33ca
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-04-16 01:43:10 +0700

    asdfsdf

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TestView
+from .views import TestView, UserProfile
 
 urlpatterns = [
     path('get_data/', TestView.as_view(), name='test get data'),
+    path('user_profile/<int:pk>', UserProfile.as_view())
 ]
